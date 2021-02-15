3dcb02010b7523af7a7e924b435f0ed89d9cbba6
Horacio Valdes <horavaldesg@gmail.com>
2021-02-15 15:03:04 -0500

    Paddles

---
+++ Assets/Scripts/Paddles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LeftPaddle()
+    {
+        transform.Rotate(0, 0, 30);
+        returnPaddle();
+    }
+    public void RightPaddle()
+    {
+        transform.Rotate(0, 0, -30);
+        returnPaddle();
+    }
+    IEnumerator returnPaddle()
+    {
+        yield return new WaitForSeconds(0.5f);
+        transform.Rotate(0, 0, 0);
+    }
+}
---
+++ Assets/Scripts/SliderForce.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderForce : MonoBehaviour
+{
+    Slider slider;
+    [SerializeField]private float forceNormal;
+    private float addForce;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(addForce);
+        //Debug.Log(slider.value);
+
+       
+    }
+    public void ValueChangeCheck()
+    {
+        addForce = slider.value * forceNormal;
+
+        Debug.Log(slider.value);
+    }
+
+
+}
